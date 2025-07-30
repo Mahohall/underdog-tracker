9ce00f07d1b0ab67862840b6d36a198e966bf5a5
Mahohall <mahohall@yahoo.co.uk>
2025-07-31 00:00:17 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import UnderdogSignalList from "./components/UnderdogSignalList";
+
+const App = () => (
+  <div className="p-4">
+    <h1 className="text-2xl font-bold mb-4">Underdog Tracker</h1>
+    <UnderdogSignalList />
+  </div>
+);
+
+export default App;
