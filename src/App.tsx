07e0562772a780c2b9308a6317bac97f15e8b146
Mahohall <mahohall@yahoo.co.uk>
2025-07-31 00:03:55 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Underdog Tracker</h1>
+      <p>This is the live version of the app with real-time odds and simulator support coming next.</p>
+    </div>
+  );
+}
+
+export default App;
