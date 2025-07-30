4ce1b43637b4bdfa6d78649bd0aaa87b18539566
Mahohall <mahohall@yahoo.co.uk>
2025-07-30 23:14:08 +0100

    Delete src directory

--- src/services/newsScannerService.ts
+++
@@ -1,7 +0,0 @@
-export async function fetchNews(query: string) {
-  const response = await fetch(
-    `https://news.google.com/rss/search?q=${encodeURIComponent(query)}&hl=en-GB&gl=GB&ceid=GB:en`
-  );
-  const text = await response.text();
-  return text;
-}
