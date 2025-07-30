033614d5594573a61d4d4bdd303fa3c7d36488ad
Mahohall <mahohall@yahoo.co.uk>
2025-07-30 23:17:53 +0100

    Clean upload of fresh project files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1>⚽ Underdog Tracker</h1>
+      <p>Basic app skeleton is working!</p>
+    </div>
+  )
+}
+
+export default App
